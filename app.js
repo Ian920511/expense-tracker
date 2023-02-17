3195592282b34bdc5e9f7d25752ebacd5e5116fd
Ian_Lin <ian920511@gmail.com>
2023-02-17 20:02:50 +0800

    feat: add mongoose and datatype

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 app.get("/", (req, res) => {
   res.send("hello");
 });
