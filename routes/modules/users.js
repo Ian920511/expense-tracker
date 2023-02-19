634ef8c5639506bbce1a1c6e17669b3bad16e3b4
Ian_Lin <ian920511@gmail.com>
2023-02-19 21:35:41 +0800

    feat: add user router

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -9,16 +9,47 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
+  const { account, name, password, confirmPassword } = req.body;
 
-})
+  User.findOne({ account }).then((user) => {
+    if (user) {
+      console.log("此 Email 已被註冊過了!");
+      return res.render("register", {
+        account,
+        name,
+        password,
+        confirmPassword,
+      });
+    }
+
+    return User.create({
+      account,
+      name,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+    })
+      .then(() => res.redirect("/users/login"))
+      .catch((error) => console.log(error));
+  });
+});
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.post('/login', (req, res) => {
-  
-})
+router.post(
+  "/login",
+  (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.redirect("/user/login");
+    }
+    next();
+  },
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/user/login",
+  })
+);
 
 module.exports = router;
